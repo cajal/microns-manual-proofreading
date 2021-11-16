b7ce4dd98a5488de74791dc6f6e6db887c5e5c14
spapa013 <stelios@spapa.us>
2021-11-16 10:05:12 -0600

    adds microns-utils dependency

--- python/microns-manual-proofreading-api/setup.py
+++ python/microns-manual-proofreading-api/setup.py
@@ -12,5 +12,6 @@ setup(
     version=__version__,
     description="api for microns-manual-proofreading",
     author="Stelios Papadopoulos",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=['microns-utils@git+https://github.com/cajal/microns-utils.git']
 )
