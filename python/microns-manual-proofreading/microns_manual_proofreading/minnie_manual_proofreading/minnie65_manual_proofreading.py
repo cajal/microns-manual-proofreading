10d48403514de226a87e27cbab80a836fab0c606
spapa013 <stelios@spapa.us>
2021-12-21 11:11:35 -0600

    import module from api.schemas

--- python/microns-manual-proofreading/microns_manual_proofreading/minnie_manual_proofreading/minnie65_manual_proofreading.py
+++ python/microns-manual-proofreading/microns_manual_proofreading/minnie_manual_proofreading/minnie65_manual_proofreading.py
@@ -1,15 +1,7 @@
+"""
+Methods for filling DataJoint tables in minnie65_manual_proofreading.
+"""
 import datajoint as dj
-from datajoint import datajoint_plus as djp
+import datajoint.datajoint_plus as djp
 
-import numpy as np
-import pandas as pd
-
-from microns_manual_proofreading_api import config
-schema_obj = config.SCHEMAS.MINNIE65_MANUAL_PROOFREADING
-
-config.register_adapters(schema_obj, context=locals())
-config.register_externals(schema_obj)
-
-schema = dj.schema(schema_obj.value)
-schema.spawn_missing_classes()
-schema.connection.dependencies.load()
+from microns_manual_proofreading_api.schemas import minnie65_manual_proofreading as m65manprf
