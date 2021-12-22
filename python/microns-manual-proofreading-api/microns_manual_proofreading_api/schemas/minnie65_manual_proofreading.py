e56763b57c18981166021bcc51f9b5f09c39fe38
spapa013 <stelios@spapa.us>
2021-12-22 13:25:04 -0600

    rename config import

--- python/microns-manual-proofreading-api/microns_manual_proofreading_api/schemas/minnie65_manual_proofreading.py
+++ python/microns-manual-proofreading-api/microns_manual_proofreading_api/schemas/minnie65_manual_proofreading.py
@@ -4,9 +4,9 @@ DataJoint tables for documenting manual proofreading in minnie65.
 import datajoint as dj
 import datajoint.datajoint_plus as djp
 
-from ..config import minnie65_manual_proofreading_config
+from ..config import minnie65_manual_proofreading_config as config
 
-minnie65_manual_proofreading_config.register_externals()
-minnie65_manual_proofreading_config.register_adapters(context=locals())
+config.register_externals()
+config.register_adapters(context=locals())
 
-schema = dj.schema(minnie65_manual_proofreading_config.schema_name, create_schema=True)
+schema = dj.schema(config.schema_name, create_schema=True)
