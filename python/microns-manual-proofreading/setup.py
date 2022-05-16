07a243314500c97e364d1880ad4a8045d1c627fd
spapa013 <stelios@spapa.us>
2022-05-16 03:43:59 -0500

    update setup.py to take requirements.txt

--- python/microns-manual-proofreading/setup.py
+++ python/microns-manual-proofreading/setup.py
@@ -2,15 +2,18 @@
 from setuptools import setup, find_packages
 from os import path
 
+def find_api(name):
+    return f"{name} @ file://localhost/{here}/../{name}#egg={name}"
+
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, '..', 'version.py')) as f:
     exec(f.read())
 
-def find_api(name):
-    return f"{name} @ file://localhost/{here}/../{name}#egg={name}"
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().split()
 
-api = find_api('microns-manual-proofreading-api')
+requirements += [find_api('microns-manual-proofreading-api')]
 
 setup(
     name='microns-manual-proofreading',
@@ -19,5 +22,5 @@ setup(
     author='Stelios Papadopoulos',
     author_email='spapadop@bcm.edu',
     packages=find_packages(exclude=[]),
-    install_requires=['numpy', 'scipy', 'tqdm', 'pandas', 'seaborn', 'matplotlib', api, 'microns-utils@git+https://github.com/cajal/microns-utils.git']
+    install_requires=requirements
 )
