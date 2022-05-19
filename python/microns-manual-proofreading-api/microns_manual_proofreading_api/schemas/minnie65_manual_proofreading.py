70dc5ac5e4662f1797013d6c1fe32f06baf7119b
spapa013 <stelios@spapa.us>
2022-05-19 06:23:08 -0500

    adds tables for manually proofread nuclei

--- python/microns-manual-proofreading-api/microns_manual_proofreading_api/schemas/minnie65_manual_proofreading.py
+++ python/microns-manual-proofreading-api/microns_manual_proofreading_api/schemas/minnie65_manual_proofreading.py
@@ -1,12 +1,99 @@
 """
 DataJoint tables for documenting manual proofreading in minnie65.
 """
+
+import datajoint_plus as djp
 import datajoint as dj
-import datajoint.datajoint_plus as djp
+
+from microns_materialization_api.schemas import minnie65_materialization as m65mat
 
 from ..config import minnie65_manual_proofreading_config as config
 
 config.register_externals()
 config.register_adapters(context=locals())
 
-schema = dj.schema(config.schema_name, create_schema=True)
+schema = djp.schema(config.schema_name, create_schema=True)
+
+@schema
+class ImportMethod(djp.Lookup):
+    hash_name = 'import_method'
+    definition = """
+    import_method : varchar(10) # import method hash
+    """
+
+    class ExcelPrfSheet(djp.Part):
+        enable_hashing=True
+        hash_name = 'import_method'
+        hashed_attrs = 'version', 'path_to_csv'
+        definition = """
+        -> master
+        ---
+        version : int # method version
+        path_to_csv : varchar(1000) # path to csv file
+        """
+
+@schema
+class PrfNucleusSet(djp.Lookup):
+    hash_name = 'prf_nuc_set'
+    definition = """
+    prf_nuc_set : varchar(12) # set hash
+    """
+    @classmethod
+    def restrict_one_store(cls, part_restr={}, **kwargs):
+        return cls.restrict_one_part_with_hash(part_restr=part_restr, include_parts=cls.ExcelPrfSheet, **kwargs)
+    
+    r1s = restrict_one_store
+
+    @classmethod
+    def restrict_one_maker(cls, part_restr={}, **kwargs):
+        return cls.restrict_one_part_with_hash(part_restr, include_parts=cls.ExcelPrfSheetMaker, **kwargs)
+    
+    r1m = restrict_one_maker
+
+    @classmethod
+    def restrict_one_store_with_hash(cls, hash, **kwargs):
+        return cls.restrict_one_part_with_hash(hash=hash, include_parts=cls.ExcelPrfSheet, **kwargs)
+    
+    r1swh = restrict_one_store_with_hash
+
+    @classmethod
+    def restrict_one_maker_with_hash(cls, hash, **kwargs):
+        return cls.restrict_one_part_with_hash(hash=hash, include_parts=cls.ExcelPrfSheetMaker, **kwargs)
+    
+    r1mwh = restrict_one_maker_with_hash
+
+    class ExcelPrfSheet(djp.Part):
+        hash_name = 'prf_nuc_set'
+        definition = """
+        -> m65mat.Nucleus
+        -> master
+        ---
+        excel_id : int # id of nucleus on excel sheet
+        area : varchar(10) # visual area
+        proofreader_den=NULL : varchar(450) # name of dendrite proofreader
+        time_min_den=NULL : float # time to complete dendrites (min)
+        notes_den=NULL : varchar(1000) # dendrites note
+        finished_den : tinyint # dendrite finished (bool)
+        date_finished_den=NULL : varchar(48) # date dendrite finished
+        proofreader_ax=NULL : varchar(450) # name of axon proofreader
+        time_min_ax=NULL : float # time to complete axon (min)
+        notes_ax=NULL : varchar(1000) # axon notes 
+        finished_ax : tinyint # axon finished (bool)
+        date_finished_ax=NULL : varchar(48) # date axon finished
+        axon_in_white_matter=NULL :  tinyint # 1 if axon passes into white matter (bool)
+        """
+
+    class ExcelPrfSheetMaker(djp.Part, dj.Computed):
+        enable_hashing = True
+        hash_name = 'prf_nuc_set'
+        hashed_attrs = 'nucleus_id', 'import_method'
+        hash_group = True
+        definition = """
+        -> master.ExcelPrfSheet
+        -> ImportMethod
+        ---
+        ts_inserted : timestamp # timestamp inserted
+        """
+
+schema.spawn_missing_classes()
+schema.connection.dependencies.load()
--- python/microns-manual-proofreading/microns_manual_proofreading/minnie_manual_proofreading/minnie65_manual_proofreading.py
+++ python/microns-manual-proofreading/microns_manual_proofreading/minnie_manual_proofreading/minnie65_manual_proofreading.py
@@ -1,7 +1,88 @@
 """
 Methods for filling DataJoint tables in minnie65_manual_proofreading.
 """
-import datajoint as dj
-import datajoint.datajoint_plus as djp
 
-from microns_manual_proofreading_api.schemas import minnie65_manual_proofreading as m65manprf
+import datajoint_plus as djp
+import pandas as pd
+from pathlib import Path
+import re 
+
+from microns_utils.datetime_utils import current_timestamp_for_mysql
+from microns_manual_proofreading_api.schemas import minnie65_manual_proofreading as m65mprf
+
+schema = m65mprf.schema
+config = m65mprf.config
+
+logger = djp.getLogger(__name__)
+
+class ImportMethod(m65mprf.ImportMethod):
+    @classmethod
+    def run(cls, key):
+        return cls.r1p(key).run(**key)
+
+    class ExcelPrfSheet(m65mprf.ImportMethod.ExcelPrfSheet):
+        def run(self, **kwargs):
+            params = self.fetch1()
+            if params['version'] == 1:
+                csv_path = Path(params['path_to_csv'])
+                if not csv_path.exists():
+                    msg = f'csv file does not exist at path: {str(csv_path)}.'
+                    self.Log('error', msg)
+                    raise FileNotFoundError(msg)
+
+                df = pd.read_csv(csv_path)
+                df = df.rename(columns={'#': 'excel_id'})
+                renamed_columns = [re.sub(r'\W+', '', s.lower().replace(' ', '_')).strip('_') for s in df.columns]
+                df = df.rename(columns = {a: r for a, r in zip(df.columns, renamed_columns)})
+                df = df.merge(df.nucleus_id.dropna(), how='right').drop_duplicates()
+                df = df.loc[:, :'axon_in_white_matter']
+                df = df.rename(columns={'proofreader': 'proofreader_den',
+                                    'date_finished_den_ctrl': 'date_finished_den', 
+                                        'notes':'notes_den',
+                                        'notes1': 'notes_ax',
+                                        'proofreader1': 'proofreader_ax',
+                                    'date_finished_ax_ctrl': 'date_finished_ax', 'description': 'area'})
+                df = df[[
+                    'excel_id',
+                    'nucleus_id', 
+                    'area', 
+                    'proofreader_den', 
+                    'time_min_den', 
+                    'notes_den', 
+                    'finished_den', 
+                    'date_finished_den', 
+                    'proofreader_ax', 
+                    'time_min_ax', 
+                    'notes_ax', 
+                    'finished_ax', 
+                    'date_finished_ax', 
+                    'axon_in_white_matter'
+                ]]
+                df.loc[:, 'finished_ax'] = df.finished_ax.apply(lambda x: 1*eval(x.title()))
+                df.loc[:, 'finished_den'] = df.finished_den.apply(lambda x: 1*x)
+                df.loc[:, 'axon_in_white_matter'] = df.axon_in_white_matter.apply(lambda x: 1*x)
+                df.loc[:75, 'proofreading_method'] = 'whole_cell'
+                df.loc[76:145, 'proofreading_method'] = 'projection_only'
+                df.loc[146:, 'proofreading_method'] = 'whole_cell'
+                df = df.query('finished_den == True and finished_ax == True')
+                df = df.where(pd.notnull(df), None)
+                df['ts_inserted'] = str(current_timestamp_for_mysql('US/Central'))
+                df['import_method'] = params['import_method']
+                return {'df': df}
+
+
+class PrfNucleusSet(m65mprf.PrfNucleusSet):
+
+    class ExcelPrfSheet(m65mprf.PrfNucleusSet.ExcelPrfSheet):
+        pass
+
+    class ExcelPrfSheetMaker(m65mprf.PrfNucleusSet.ExcelPrfSheetMaker):
+        @property
+        def key_source(self):
+            return ImportMethod.ExcelPrfSheet
+
+        def make(self, key):
+            df = ImportMethod.run(key)['df']
+            df['prf_nuc_set'] = self.hash1(df, unique=True)
+            self.master.ExcelPrfSheet.insert(df, ignore_extra_fields=True, insert_to_master=True, insert_to_master_kws={'ignore_extra_fields': True, 'skip_duplicates': True})
+            self.insert(df, ignore_extra_fields=True, skip_hashing=True)
